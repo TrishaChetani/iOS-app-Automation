c3469e1cf589f56b65eb5b0de38e80980ab3cdb8
Trisha <agarwalatrisha1212@gmail.com>
2017-06-27 01:30:01 +0800

    First automated test

---
+++ test/spec/test.spec.js
@@ -0,0 +1,8 @@
+describe('Egami Automation', function () {
+    it('should launch the app and click on continue from splash screen', function () {
+    	browser.click(`~CONTINUE`);
+   		browser.click(`~CONTINUE`);
+        browser.click(`~CONTINUE`);
+        browser.pause(2000);
+    });
+});
